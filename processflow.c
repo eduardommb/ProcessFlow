30e0bb5c12c1190bb49c6394b2f9b16d6153021a
eduardommb <lemb@cesar.school>
2026-08-18 20:11:39 -0300

    test: processflow>

--- processflow.c
+++ processflow.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
 int main() {
+
+    char linha[256];
+
+    printf("processflow> ");
+    fflush(stdout);
+    fgets(linha, sizeof(linha), stdin);
+
+    printf("%s", linha);
+
     return 0;
 }
